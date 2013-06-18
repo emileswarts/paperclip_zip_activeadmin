411946705a19982c86635f06a2477d3fafef2bce
Emile Swarts <emile.swarts123@gmail.com>
2013-06-18 08:52:54 +0100

    some tests

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -4,26 +4,43 @@ class ProductImage < ActiveRecord::Base
 
   belongs_to :product
 
-  has_attached_file :zip, :url => "/tmp/:id/:basename.:extension"
-  after_post_process :extract
+  has_attached_file :zip, :path => ":rails_root/public/tmp/:id/:basename.:extension"
+
+  after_save :extract
+  # after_post_process :extract
   # validates_attachment_content_type :zip, :content_type => ['zip']
 
   def extract
     zip_file_path = "#{Rails.root}/public/tmp/#{self.id}/#{self.zip_file_name}"
-     Zip::ZipFile.open(zip_file_path) do |zipfile|
-     #   zipfile.each do |file|
-     #     export_path = "#{Rails.root}/assets/sequnces/"
-     #     image_path = File.join(export_path, zip_file_name)
-     #     100.times do
-     #       p image_path
-     #     end
-     #       FileUtils.mkdir_p(File.dirname(image_path))
-     #       unless File.exist?(image_path)
-     #         zip_file.extract(image, image_path)
-     #         # ..stuff that it does..
-     #       end
-     #   end
-     end
+    # 100.times { p File.exist?(zip_file_path) }
+
+    Zip::ZipFile.open(zip_file_path) do |zipfile|
+      zipfile.each do |file|
+        export_path = "#{Rails.root}/public/tmp/#{self.id}_2/"
+        zip_path = File.join(export_path, zip_file_name)
+
+        100.times do
+          p zip_path
+        end
+
+        # export_path = "#{Rails.root}/public/tmp/#{self.id}_2/"
+        # zip_path = File.join(export_path, zip_file_name)
+        # FileUtils.mkdir_p(File.dirname(export_path))
+
+        # unless File.exist?(zip_file_path)
+        #    zipfile.extract(file, zip_path)
+        # #   # ..stuff that it does..
+        # end
+        #     100.times do
+        #       p image_path
+        #     end
+        #       FileUtils.mkdir_p(File.dirname(image_path))
+        #       unless File.exist?(image_path)
+        #         zip_file.extract(image, image_path)
+        #         # ..stuff that it does..
+        #       end
+      end
+    end
   end
 
   def myrand
